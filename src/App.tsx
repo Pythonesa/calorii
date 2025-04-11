4da38be73ee2738d0c37dc4320004de7a0612eb6
Pythonesa <pythonesamar@gmail.com>
2025-04-11 13:45:23 -0300

    💄 add header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ function App() {
 
   return (
     <>
-      <h1>Calorii</h1>
+      <header>
+        <div id="logo">
+          <img src="assets/img/favicon.png" alt="Calorii" />
+          <h1>Calorii</h1>
+        </div>
+        <button id="reset" type="button">Reiniciar</button>
+      </header>
     </>
   )
 }
